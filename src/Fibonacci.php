c17308e2905d9989cc588b9afa761b7189ace646
Youngjoon Park <pyj2025@gmail.com>
2019-02-13 11:28:09 -0500

    Math class commit

---
+++ src/Fibonacci.php
@@ -0,0 +1,9 @@
+<?php
+    namespace Sfp;
+
+    class Fibonacci {    
+        
+
+    }
+
+?>
---
+++ src/Math.php
@@ -0,0 +1,65 @@
+<?php
+    namespace Sfp;
+
+    class Math {    
+
+        private $result;
+
+        public function __construct() {
+            $this->result = 0;
+        }
+
+        function execute() { 
+            $handle = fopen("../asset/tab.csv", "r"); 
+            
+            $row = 1;
+            $counter = 0;
+            $sum = 0;
+            $avg = 0;
+           
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) { 
+                $num = count($data); 
+               
+                for ($c = 0; $c < $num; $c++) { 
+                    // check first line
+                    if($row == 1) {
+                        // check second col and third col is right
+                        if($data[1] === "value" && $data[2] === "accept") {
+                            continue;
+                        }
+                        else {
+                            echo "Error";
+                            break;
+                        }
+                    }
+                    else {
+                        if($data[2] === "TRUE") {
+                            $counter++;
+                            $sum += $data[1];
+
+                            break;
+                        }
+                    }
+                    
+                }
+                
+                $row++; 
+            }
+            
+            fclose($handle); 
+
+            $avg = $sum / $counter; # get the average 
+            $avg = doubleval($avg); # make it double
+            $result = (double) number_format($avg, 10, '.', ''); # 10 decimals
+           
+            return $result;
+        }
+    }
+
+    // Test function
+    // $obj = new Math();
+    // var_dump($obj->execute());
+    // echo "<br \>";
+    // print($obj->execute());
+   
+?>
