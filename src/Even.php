de1962cac1014ce08fdf5c29d9d7cccba0125385
Youngjoon Park <pyj2025@gmail.com>
2019-02-13 09:57:38 -0500

    Even class commit

---
+++ src/Even.php
@@ -0,0 +1,37 @@
+<?php
+    namespace Sfp;
+
+    class Even {
+
+        function execute() { 
+            $handle = fopen("../assets/numbers.csv", "r"); 
+            $counter = 0; #counts number of evens
+            
+            //get data from file
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) { 
+                $numData = count($data); #num data in line
+
+                for ($i = 0; $i < $num; $i++) { 
+                    //check the number is even or not
+                    if($data[$i] % 2 == 0) { 
+                        //increase if number is even
+                        $counter++; 
+                    } 
+                } 
+            } 
+
+            fclose($handle); 
+
+            // echo $counter;
+            return $counter;
+        }
+
+    }
+
+    // Test function
+    // $obj = new Even();
+    // var_dump($obj->execute());
+    // echo "<br \>";
+    // print($obj->execute());
+
+?>
